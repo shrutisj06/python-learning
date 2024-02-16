c3acdbd98b25acd69b5620fdbd7257c69c6bf7fd
shrutisj06 <157938791+shrutisj06@users.noreply.github.com>
2024-02-16 19:36:32 +0530

    Add files via upload

---
+++ break and continue.py
@@ -0,0 +1,16 @@
+#continue
+for i in range (1,10,1):
+     if (i==8):
+        continue
+     else:
+        print(i)
+
+
+#break
+for i in range (0,50,4):
+    if (i==32):
+        break
+    else:
+        print(i)
+    
+
